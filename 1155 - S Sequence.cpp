438900c449877ffa85d08c5f91182481d683ef5f
sbf-rusho <81906980+sbf-rusho@users.noreply.github.com>
2021-05-04 15:08:43 +0600

    Create  1155 - S Sequence.cpp

---
+++ 1155 - S Sequence.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int i;
+    float s,sum=0.0;
+    for(i=1;i<=100;i++)
+        sum = sum+(float) 1/i;
+
+cout<<showpoint;
+cout<<fixed;
+cout<<setprecision(2);
+    cout<<sum<<endl;
+}
