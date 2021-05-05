c7ea40cd8c69ed0254d7418fc632bc1b94ade937
sbf-rusho <81906980+sbf-rusho@users.noreply.github.com>
2021-05-06 00:01:53 +0600

    Create 1160 - Population Increase.cpp

---
+++ 1160 - Population Increase.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int population(int pa,int pb,double ga,double gb)
+{
+   long long  int pi=0,c=0;
+    while(pa<=pb)
+    {
+        pa=pa+(pa*(float) ga)/100.0;
+        pb = pb + (pb*(float)gb)/100.0;
+        c++;
+    }
+    if(c>100)
+        cout<<"Mais de 1 seculo."<<endl;
+
+    else
+    cout<<c<<" "<<"anos."<<endl;
+
+}
+
+//driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int pa,pb;
+        double ga,gb;
+        cin>>pa>>pb>>ga>>gb;
+        population(pa,pb,ga,gb);
+    }
+}
